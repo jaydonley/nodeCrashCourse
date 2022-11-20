c3509b20c7ea1b575517f7c39c2444196244516b
Jason Donley <jay.donley@gmail.com>
2022-11-20 14:00:13 -0500

    finishedProject-needsReviewed

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+//-------- mongoDB: JDKA , JDKA
+
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const { render } = require('ejs');
+const blogRoutes = require('./routes/blogRoutes')
 
 const app = express();
 
+const dbURI = "mongodb+srv://KATEST:KATEST@node-tuts.vqzsbhx.mongodb.net/node-tuts?retryWrites=true&w=majority"
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
 app.set('view engine', 'ejs');
 
-app.listen(3000);
 
-app.use(morgan('dev'));
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
 
 // app.use((req, res, next) => {
 //     console.log('A New Request Made:');
@@ -16,41 +28,32 @@ app.use(express.static('public'));
 //     console.log('path: ', req.path);
 //     console.log('method: ', req.method);
 //     next();
-// })
+// });
 
 // app.use((req, res, next) => {
 //     console.log('in the next middleware');
 //     next();
 // });
 
-const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat Bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
+
+
+
 
 app.get('/', (req, res) => {
-    // res.send('<p>Home Page</p>');
-    // res.sendFile('./views/index.html', { root: __dirname });
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
-    // res.send('<p>About Page</p>');
-    // res.sendFile('./views/about.html', { root: __dirname });
     res.render('about', { title: 'About' });
 
 });
 
-// app.get('/about-us', (req, res) => {
-//     res.redirect('/about');
-// });
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create A New Blog' });
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
 });
 
+app.use('/blogs', blogRoutes);
+
 app.use((req, res) => {
-    // res.status(404).sendFile('./views/404.html', { root: __dirname });
     res.status(404).render('404', { title: '404' });
 });
